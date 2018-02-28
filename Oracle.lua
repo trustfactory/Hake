d8200c58b478444e0bd8941966cb74a674ff270c
PuckItYOLO <35457186+PuckItYOLO@users.noreply.github.com>
2018-02-28 06:13:29 -0800

    Added hotkey to enable/disable auto mode

--- Oracle.lua
+++ Oracle.lua
@@ -4,20 +4,36 @@ local Oracle = {}
 
 local optionAutoSave = Menu.AddOption({"Hero Specific", "Oracle"}, "Auto Save", "Auto cast 'False Promise' to save needed ally")
 local optionAutoHeal = Menu.AddOption({"Hero Specific", "Oracle"}, "Auto Heal", "Auto cast 'Purifying Flames' to heal ally")
+local key = Menu.AddKeyOption({"Hero Specific", "Oracle"}, "Activate Auto Spells Key", Enum.ButtonCode.KEY_S)
 local font = Renderer.LoadFont("Tahoma", 30, Enum.FontWeight.EXTRABOLD)
+local inAutoSpellsMode = false
 
 function Oracle.OnUpdate()
     local myHero = Heroes.GetLocal()
     if not myHero then return end
     if not Utility.IsSuitableToCastSpell(myHero) then return end
     
-    if Menu.IsEnabled(optionAutoSave) then
+    if Menu.IsKeyDownOnce(key) then
+        inAutoSpellsMode = not inAutoSpellsMode
+    end
+    
+    if Menu.IsEnabled(optionAutoSave) and inAutoSpellsMode then
         Oracle.AutoSave(myHero)
     end
 
-    if Menu.IsEnabled(optionAutoHeal) then
+    if Menu.IsEnabled(optionAutoHeal) and inAutoSpellsMode then
         Oracle.AutoHeal(myHero)
     end
+end
+
+function Oracle.OnDraw()
+    local myHero = Heroes.GetLocal()
+    if not myHero or NPC.GetUnitName(myHero) ~= "npc_dota_hero_oracle" then return end
+    if not inAutoSpellsMode then return end
+    local pos = Entity.GetAbsOrigin(myHero)
+    local x, y, visible = Renderer.WorldToScreen(pos)
+    Renderer.SetDrawColor(255, 255, 0, 255)
+    Renderer.DrawTextCentered(font, x, y, "I Know De Wey", 1)
 end
 
 function Oracle.AutoSave(myHero)
