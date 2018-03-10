33812b4be9e78bd5760281f5431549ac74829a57
PuckItYOLO <35457186+PuckItYOLO@users.noreply.github.com>
2018-03-10 07:56:08 -0800

    Overhauled to Combo Key Usage

--- Oracle.lua
+++ Oracle.lua
@@ -1,75 +1,66 @@
-local Utility = require("Utility")
-
 local Oracle = {}
 
-local optionAutoSave = Menu.AddOption({"Hero Specific", "Oracle"}, "Auto Save", "Auto cast 'False Promise' to save needed ally")
-local optionAutoHeal = Menu.AddOption({"Hero Specific", "Oracle"}, "Auto Heal", "Auto cast 'Purifying Flames' to heal ally")
-local key = Menu.AddKeyOption({"Hero Specific", "Oracle"}, "Activate Auto Spells Key", Enum.ButtonCode.KEY_S)
-local font = Renderer.LoadFont("Tahoma", 30, Enum.FontWeight.EXTRABOLD)
-local inAutoSpellsMode = false
+Oracle.optionKey = Menu.AddKeyOption({"Hero Specific","Oracle"}, "Non-Ult Healing Key", Enum.ButtonCode.KEY_D)
+Oracle.optionKey2 = Menu.AddKeyOption({"Hero Specific","Oracle"}, "Ult Healing Key", Enum.ButtonCode.KEY_F)
+Oracle.optionEnable = Menu.AddOption({"Hero Specific","Oracle"}, "Enabled", "Enable Or Disable Oracle Combo Script")
 
 function Oracle.OnUpdate()
+    if not Menu.IsEnabled(Oracle.optionEnable) then return true end
+	if Menu.IsKeyDown(Oracle.optionKey)then
+    Oracle.Combo()
+	end
+	
+	if not Menu.IsEnabled(Oracle.optionEnable) then return true end
+	if Menu.IsKeyDown(Oracle.optionKey2)then
+    Oracle.Combo2()
+	end
+end	
+
+
+function Oracle.Combo()
+if not Menu.IsKeyDown(Oracle.optionKey) then return end
     local myHero = Heroes.GetLocal()
-    if not myHero then return end
-    if not Utility.IsSuitableToCastSpell(myHero) then return end
-    
-    if Menu.IsKeyDownOnce(key) then
-        inAutoSpellsMode = not inAutoSpellsMode
-    end
-    
-    if Menu.IsEnabled(optionAutoSave) and inAutoSpellsMode then
-        Oracle.AutoSave(myHero)
-    end
-
-    if Menu.IsEnabled(optionAutoHeal) and inAutoSpellsMode then
-        Oracle.AutoHeal(myHero)
-    end
-end
-
-function Oracle.OnDraw()
-    local myHero = Heroes.GetLocal()
-    if not myHero or NPC.GetUnitName(myHero) ~= "npc_dota_hero_oracle" then return end
-    if not inAutoSpellsMode then return end
-    local pos = Entity.GetAbsOrigin(myHero)
-    local x, y, visible = Renderer.WorldToScreen(pos)
-    Renderer.SetDrawColor(255, 255, 0, 255)
-    Renderer.DrawTextCentered(font, x, y, "I Know De Wey", 1)
-end
-
-function Oracle.AutoSave(myHero)
-    local promise = NPC.GetAbility(myHero, "oracle_false_promise")
+    if NPC.GetUnitName(myHero) ~= "npc_dota_hero_oracle" then return end
+    local hero = Input.GetNearestHeroToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_FRIEND)
     local mana = NPC.GetMana(myHero)
-    local promiserange = Ability.GetCastRange(promise)
-    for i = 1, Heroes.Count() do
-    local ally = Heroes.Get(i)
-    if ally and not NPC.IsIllusion(ally) and Entity.IsSameTeam(myHero, ally) and NPC.IsEntityInRange(myHero, ally, promiserange) and Utility.CanCastSpellOn(ally) and Entity.GetHealth(ally) <= Entity.GetMaxHealth(ally) * 0.3 and Ability.IsReady(promise) and Ability.IsCastable(promise, math.floor(mana)) then
-    Ability.CastTarget(promise, ally)
-    return end
-    
-    local myHealth = Entity.GetHealth(myHero)
-
-    if myHealth <= Entity.GetMaxHealth(myHero) * 0.3 and Ability.IsReady(promise) and Ability.IsCastable(promise, math.floor(mana)) then
-    Ability.CastTarget(promise, myHero)
-    return end
+	
+	if not hero then return end
+	
+    local edict = NPC.GetAbility(myHero, "oracle_fates_edict")
+    local flames = NPC.GetAbility(myHero, "oracle_purifying_flames")
+	
+	if Menu.IsEnabled(Oracle.optionEnable) then
+		     
+	if not NPC.HasState(hero, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE)
+	and edict and Ability.IsCastable(edict, mana) then Ability.CastTarget(edict, hero) return 
+	end
+			     
+	if not NPC.HasState(hero, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE)
+	and flames and Ability.IsCastable(flames, mana) then Ability.CastTarget(flames, hero) return end
 	end
 end
 
-function Oracle.AutoHeal(myHero)
-    local flames = NPC.GetAbility(myHero, "oracle_purifying_flames")
+function Oracle.Combo2()
+if not Menu.IsKeyDown(Oracle.optionKey2) then return end
+    local myHero = Heroes.GetLocal()
+    if NPC.GetUnitName(myHero) ~= "npc_dota_hero_oracle" then return end
+    local hero = Input.GetNearestHeroToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_FRIEND)
     local mana = NPC.GetMana(myHero)
-    local flamesrange = Ability.GetCastRange(flames)
-    for i = 1, Heroes.Count() do
-    local ally = Heroes.Get(i)
-    if ally and not NPC.IsIllusion(ally) and Entity.IsSameTeam(myHero, ally) and NPC.IsEntityInRange(myHero, ally, flamesrange) and Utility.CanCastSpellOn(ally) and Entity.GetHealth(ally) <= Entity.GetMaxHealth(ally) * 0.3 and Ability.IsReady(flames) and Ability.IsCastable(flames, math.floor(mana)) then
-    Ability.CastTarget(flames, ally)
-    return end
-    
-    local myHealth = Entity.GetHealth(myHero)
-
-    if myHealth <= Entity.GetMaxHealth(myHero) * 0.3 and Ability.IsReady(flames) and Ability.IsCastable(flames, math.floor(mana)) then
-    Ability.CastTarget(flames, myHero)
-    return end
-	end  
+	
+	if not hero then return end
+	
+    local promise = NPC.GetAbility(myHero, "oracle_false_promise")
+    local flames = NPC.GetAbility(myHero, "oracle_purifying_flames")
+	
+	if Menu.IsEnabled(Oracle.optionEnable) then
+		     
+	if not NPC.HasState(hero, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE)
+	and promise and Ability.IsCastable(promise, mana) then Ability.CastTarget(promise, hero) return 
+	end
+			     
+	if not NPC.HasState(hero, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE)
+	and flames and Ability.IsCastable(flames, mana) then Ability.CastTarget(flames, hero) return end
+	end
 end
-
+	
 return Oracle
