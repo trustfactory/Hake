06fc6fd9aa346301eecfacb571411472ff94b176
PuckItYOLO <35457186+PuckItYOLO@users.noreply.github.com>
2018-05-16 21:15:51 -0700

    Updated with targeting changes

--- ShadowDemon.lua
+++ ShadowDemon.lua
@@ -6,23 +6,31 @@ Shadow.poisonKey = Menu.AddKeyOption({"Hero Specific","Shadow Demon"},"3. Poison
 Shadow.comboKey = Menu.AddKeyOption({"Hero Specific","Shadow Demon"},"2. Combo Key",Enum.ButtonCode.KEY_D)
 Shadow.optionEnable = Menu.AddOption({"Hero Specific","Shadow Demon"},"1. Enabled","Enable/Disable Shadow Demon Combo")
 Shadow.optionSDBlink = Menu.AddOption({ "Hero Specific", "Shadow Demon" }, "4. Use Blink to Initiate {{Shadow Demon}}", "")
-Shadow.optionSDBlinkRange = Menu.AddOption({ "Hero Specific", "Shadow Demon" }, "5. Set Safe Blink Initiation Range {{Shadow Demon}}", "If over 575, then Euls will not activate in combo", 200, 800, 25)
+Shadow.optionSDBlinkRange = Menu.AddOption({ "Hero Specific", "Shadow Demon" }, "5. Set Safe Blink Initiation Range {{Shadow Demon}}", "If over 575, then Euls will not activate in combo, if Lens (+250 to your set Blink Range)", 200, 800, 25)
 --Skills Toggle Menu--
 Shadow.optionEnableDisruption = Menu.AddOption({ "Hero Specific","Shadow Demon","6. Skills"},"1. Use Disruption","Enable Or Disable")
 Shadow.optionEnableCatcher = Menu.AddOption({ "Hero Specific","Shadow Demon","6. Skills"},"2. Use Catcher","Enable Or Disable")
 Shadow.optionEnablePoison = Menu.AddOption({ "Hero Specific","Shadow Demon","6. Skills"},"3. Use Poison","Enable Or Disable")
 Shadow.optionEnableUlt = Menu.AddOption({ "Hero Specific","Shadow Demon","6. Skills"},"4. Use Ult","Enable Or Disable")
 --Items Toggle Menu--
-Shadow.optionEnableGlimmer = Menu.AddOption({ "Hero Specific","Shadow Demon","7. Items"},"1. Use Glimmer Cape","Turn On/Off Glimmer Cape in Combo")
-Shadow.optionEnableEuls = Menu.AddOption({ "Hero Specific","Shadow Demon","7. Items"},"2. Use Euls","Turn On/Off Euls in Combo")
+Shadow.optionEnableAtos = Menu.AddOption({ "Hero Specific","Shadow Demon","7. Items"},"1. Use Atos","Turn On/Off Rod of Atos in Combo")
+Shadow.optionEnableGlimmer = Menu.AddOption({ "Hero Specific","Shadow Demon","7. Items"},"2. Use Glimmer Cape","Turn On/Off Glimmer Cape in Combo")
+Shadow.optionEnableEuls = Menu.AddOption({ "Hero Specific","Shadow Demon","7. Items"},"3. Use Euls","Turn On/Off Euls in Combo")
 
 -- global Variables
-Shadow.lastTick = 0
 Shadow.delay = 0
+Shadow.lastTick = 0
+Shadow.lastAttackTime = 0
+Shadow.lastAttackTime2 = 0
+Shadow.LastTarget = nil
+
 
 function Shadow.ResetGlobalVariables()
-	Shadow.lastTick = 0
 	Shadow.delay = 0
+	Shadow.lastTick = 0
+	Shadow.lastAttackTime = 0
+	Shadow.lastAttackTime2 = 0
+	Shadow.LastTarget = nil
 end
 
 
@@ -201,7 +209,7 @@ if not Menu.IsKeyDown(Shadow.comboKey) then return end
     local hero = Input.GetNearestHeroToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_ENEMY)
     local heroPos
     if hero ~= nil then
-        heroPos = Entity.GetAbsOrigin(hero)
+    heroPos = Entity.GetAbsOrigin(hero)
     end
 	local mousePos = Input.GetWorldCursorPos()
     local mana = NPC.GetMana(myHero)
@@ -223,15 +231,16 @@ if not Menu.IsKeyDown(Shadow.comboKey) then return end
     local Glimmer = NPC.GetItem(myHero, "item_glimmer_cape", true)
 	
 	--Ability Ranges--
-    local DisruptionRange = Ability.GetCastRange(Disruption)
-  	local CatcherRange = Ability.GetCastRange(Catcher)
-  	local PoisonRange = Ability.GetCastRange(Poison)
-  	local UltRange = Ability.GetCastRange(Ult)
+    local DisruptionRange = 600
+  	local CatcherRange = 600
+  	local PoisonRange = 1500
+  	local UltRange = 800
   	
   	--Item Ranges--
   	local BlinkRange = 1200
   	local AtosRange = 1150
   	local EulsRange = 575
+  	local VeilRange = 1000
     
     if Lens then
     		DisruptionRange = DisruptionRange + 250
@@ -240,7 +249,8 @@ if not Menu.IsKeyDown(Shadow.comboKey) then return end
     		UltRange = UltRange + 250
     		BlinkRange = BlinkRange + 250
     		AtosRange = AtosRange + 250
-    		EulsRange = EulsRange +250   		
+    		EulsRange = EulsRange +250
+			VeilRange = VeilRange +250   		
     end
     
 	if Menu.IsEnabled(Shadow.optionEnable) then
@@ -250,72 +260,41 @@ if not Menu.IsKeyDown(Shadow.comboKey) then return end
             Ability.CastPosition(Blink, (Entity.GetAbsOrigin(hero) + (Entity.GetAbsOrigin(myHero) - Entity.GetAbsOrigin(hero)):Normalized():Scaled(Menu.GetValue(Shadow.optionSDBlinkRange)))) Shadow.lastTick = os.clock() return end
         end
         
-        if Shadow.SleepReady(0.05) and Atos and Ability.IsReady(Atos) and Ability.IsCastable(Atos, mana) and not NPC.IsIllusion(hero) and not NPC.GetModifier(hero, "modifier_sheepstick_debuff") and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), AtosRange) then Ability.CastTarget(Atos, hero) Shadow.lastTick = os.clock() return end
+        if Shadow.SleepReady(0.05) and Utility.CanCastSpellOn(hero) and Euls and Ability.IsReady(Euls) and Menu.IsEnabled(Shadow.optionEnableEuls) and Ability.IsCastable(Euls, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), EulsRange) and not NPC.HasModifier(hero, "modifier_rod_of_atos_debuff") and not NPC.HasModifier(hero, "modifier_eul_cyclone") then Ability.CastTarget(Euls, hero) Shadow.lastTick = os.clock() return end
         
-        if Shadow.SleepReady(0.05) and Veil and Ability.IsReady(Veil) and Ability.IsCastable(Veil, mana) and not NPC.IsIllusion(hero) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), Ability.GetCastRange(Veil),0) and heroPos then Ability.CastPosition(Veil, heroPos) Shadow.lastTick = os.clock() return end
+        if Shadow.SleepReady(0.3) and Disruption and Ability.IsReady(Disruption) and Menu.IsEnabled(Shadow.optionEnableDisruption) and Ability.IsCastable(Disruption, mana) and not NPC.IsIllusion(hero) and Entity.IsAlive(hero) and not NPC.IsStructure(hero) and not NPC.HasState(hero, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), DisruptionRange) then Ability.CastTarget(Disruption, hero) Shadow.lastTick = os.clock() return end
         
-        if Shadow.SleepReady(0.3) and Disruption and Ability.IsReady(Disruption) and Menu.IsEnabled(Shadow.optionEnableDisruption) and Ability.IsCastable(Disruption, mana) and not NPC.IsIllusion(hero) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), DisruptionRange) then Ability.CastTarget(Disruption, hero) Shadow.lastTick = os.clock() return end
+        if Shadow.SleepReady(0.05) and Veil and Ability.IsReady(Veil) and Ability.IsCastable(Veil, mana) and not NPC.IsIllusion(hero) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), VeilRange) and NPC.HasModifier(hero, "modifier_shadow_demon_disruption") and heroPos and Entity.IsAlive(hero) and not NPC.IsStructure(hero) then Ability.CastPosition(Veil, heroPos) Shadow.lastTick = os.clock() return end
         
-        -- algorithm for soul catcher
-        if Shadow.SleepReady(0.3) and Catcher and Ability.IsReady(Catcher) and Menu.IsEnabled(Shadow.optionEnableCatcher) and Ability.IsCastable(Catcher, mana) and not NPC.IsIllusion(hero) and heroPos and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), CatcherRange+CatcherRange*0.5,0) then
-                  
-        -- get enemy origin
-        -- get myHero origin
-        -- get points at increments in a circle around enemy origin
-        -- if the point is within soul catcher aoe for the enemy and the point is within soul catcher cast range for myHero then
-        -- calculate how many enemies would be in the aoe
-        -- if 1, then cast
-        -- continue for all points around the circle
-        -- increase the circle radius and repeat
+        if Shadow.SleepReady(0.3) and Catcher and Ability.IsReady(Catcher) and Menu.IsEnabled(Shadow.optionEnableCatcher) and Ability.IsCastable(Catcher, mana) and not NPC.IsIllusion(hero) and not NPC.IsStructure(hero) and not NPC.HasModifier(hero, "modifier_black_king_bar_immune") and NPC.HasModifier(hero, "modifier_shadow_demon_disruption") and heroPos and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), CatcherRange+CatcherRange*0.5,0) then
+ 		Ability.CastPosition(Catcher, heroPos)
+        Shadow.lastTick = os.clock() return 
+		end
+        
+        if Shadow.SleepReady(0.3) and Poison and Ability.IsReady(Poison) and Menu.IsEnabled(Shadow.optionEnablePoison) and Ability.IsCastable(Poison, mana) and not NPC.HasModifier(hero, "modifier_eul_cyclone") and not NPC.IsIllusion(hero) and Entity.IsAlive(hero) and not NPC.IsStructure(hero) and not NPC.HasState(hero, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and heroPos and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), PoisonRange)
+		then Ability.CastPosition(Poison, heroPos) Shadow.lastTick = os.clock() return end
 
-        if not Shadow.CatcherTime then
-            Shadow.CatcherTime = 0
-        end
-
-        local myHeroPos = Entity.GetAbsOrigin(myHero)
-        local angle = Entity.GetRotation(myHero)
-        for range = 50, 600, 50 do
-            for step = 0, 360, 5 do
-                local angleOffset = Angle(0, step, 0)
-                angle:SetYaw(angle:GetYaw() + angleOffset:GetYaw())
-                local x, y, z = angle:GetVectors()
-                local direction = x + y + z
-                direction:SetZ(0)
-                direction:Normalize()
-                direction:Scale(range)
-                local newPos = myHeroPos+direction
-                Log.Write(tostring((newPos-myHeroPos):Length2D()))
-                if NPC.IsPositionInRange(myHero, newPos, 600, 0) then
-                    local unitsAround = 0
-                    for i = 1, NPCs.Count() do
-                        local npc = NPCs.Get(i)
-                        if npc and Entity.GetHealth(npc) > 0 and not Entity.IsSameTeam(myHero, npc) and NPC.IsPositionInRange(npc, newPos, 460, 0) then
-                            unitsAround = unitsAround + 1
-                        end
-                    end
-                    if unitsAround and unitsAround > 0 then
-                        if unitsAround < 2 and NPC.IsPositionInRange(hero, newPos, 400, 0) then
-                            Ability.CastPosition(Catcher, newPos)
-                            return
-                        end
-                    end
-                end
-            end
-        end
-
-        Ability.CastPosition(Catcher, heroPos)
-        Shadow.lastTick = os.clock()
-        return end
-         -- end algorithm for soul catcher
+        if Shadow.SleepReady(0.3) and Shadow.CanCastSpellOn(hero) and Ult and Ability.IsReady(Ult) and Menu.IsEnabled(Shadow.optionEnableUlt) and Ability.IsCastable(Ult, mana) and not NPC.HasModifier(hero, "modifier_eul_cyclone") and not NPC.IsIllusion(hero) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), UltRange) then Ability.CastTarget(Ult, hero) Shadow.lastTick = os.clock() return end
         
-        if Shadow.SleepReady(0.3) and Poison and Ability.IsReady(Poison) and Menu.IsEnabled(Shadow.optionEnablePoison) and Ability.IsCastable(Poison, mana) and not NPC.HasModifier(hero, "modifier_eul_cyclone") and not NPC.IsIllusion(hero) and heroPos and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), PoisonRange) then Ability.CastPosition(Poison, heroPos) Shadow.lastTick = os.clock() return end
+        if Shadow.SleepReady(0.05) and Utility.CanCastSpellOn(hero) and Atos and Ability.IsReady(Atos) and Menu.IsEnabled(Shadow.optionEnableAtos) and Ability.IsCastable(Atos, mana) and not NPC.IsIllusion(hero) and not NPC.HasModifier(hero, "modifier_sheepstick_debuff") and not NPC.HasModifier(hero, "modifier_eul_cyclone") and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), AtosRange)
+		and Ability.SecondsSinceLastUse(Disruption) > 1.7 then 
+			if not NPC.HasModifier(hero, "modifier_shadow_demon_disruption") then
+			Ability.CastTarget(Atos, hero) Shadow.lastTick = os.clock() return end
+		end
         
-        if Shadow.SleepReady(0.05) and Euls and Ability.IsReady(Euls) and Menu.IsEnabled(Shadow.optionEnableEuls) and Ability.IsCastable(Euls, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), EulsRange) and not NPC.HasState(hero, Enum.ModifierState.MODIFIER_STATE_ROOTED) then Ability.CastTarget(Euls, hero) Shadow.lastTick = os.clock() return end
-
-        if Shadow.SleepReady(0.3) and Ult and Ability.IsReady(Ult) and Menu.IsEnabled(Shadow.optionEnableUlt) and Ability.IsCastable(Ult, mana) and not NPC.HasModifier(hero, "modifier_eul_cyclone") and not NPC.HasState(hero, Enum.ModifierState.MODIFIER_STATE_INVULNERABLE) and not NPC.IsIllusion(hero) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), UltRange) then Ability.CastTarget(Ult, hero) Shadow.lastTick = os.clock() return end
-        
-        if Shadow.SleepReady(0.05) and Glimmer and Ability.IsReady(Glimmer) and Menu.IsEnabled(Shadow.optionEnableGlimmer) and Ability.IsCastable(Glimmer, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), Ability.GetCastRange(Glimmer),0) then Ability.CastTarget(Glimmer, myHero) Shadow.lastTick = os.clock() return end
+        if Shadow.SleepReady(0.05) and Glimmer and Ability.IsReady(Glimmer) and Menu.IsEnabled(Shadow.optionEnableGlimmer) and Ability.IsCastable(Glimmer, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(hero), Ability.GetCastRange(Glimmer),0) then Ability.CastTarget(Glimmer, myHero) Shadow.lastTick = os.clock() return
+		end
+		
+		Shadow.GenericMainAttack(myHero, "Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET", hero, nil) return
     end
+end
+
+function Shadow.CanCastSpellOn(npc)
+	if Entity.IsDormant(npc) or not Entity.IsAlive(npc) then return false end
+	if NPC.IsStructure(npc) or not NPC.IsKillable(npc) then return false end
+	if NPC.HasState(npc, Enum.ModifierState.MODIFIER_STATE_INVULNERABLE) then return false end
+	
+	return true
 end
 
 function Shadow.SleepReady(sleep)
@@ -331,5 +310,156 @@ function Shadow.makeDelay(sec)
 	Shadow.delay = sec + NetChannel.GetAvgLatency(Enum.Flow.FLOW_OUTGOING)
 	Shadow.lastTick = os.clock()
 end
+
+function Shadow.isHeroChannelling(myHero)
+
+	if not myHero then return true end
+
+	if NPC.IsChannellingAbility(myHero) then return true end
+	if NPC.HasModifier(myHero, "modifier_teleporting") then return true end
+
+	return false
+end
+
+function Shadow.heroCanCastItems(myHero)
+
+	if not myHero then return false end
+	if not Entity.IsAlive(myHero) then return false end
+
+	if NPC.IsStunned(myHero) then return false end
+	if NPC.HasModifier(myHero, "modifier_bashed") then return false end
+	if NPC.HasState(myHero, Enum.ModifierState.MODIFIER_STATE_INVULNERABLE) then return false end	
+	if NPC.HasModifier(myHero, "modifier_eul_cyclone") then return false end
+	if NPC.HasModifier(myHero, "modifier_obsidian_destroyer_astral_imprisonment_prison") then return false end
+	if NPC.HasModifier(myHero, "modifier_shadow_demon_disruption") then return false end	
+	if NPC.HasModifier(myHero, "modifier_invoker_tornado") then return false end
+	if NPC.HasState(myHero, Enum.ModifierState.MODIFIER_STATE_HEXED) then return false end
+	if NPC.HasModifier(myHero, "modifier_legion_commander_duel") then return false end
+	if NPC.HasModifier(myHero, "modifier_axe_berserkers_call") then return false end
+	if NPC.HasModifier(myHero, "modifier_winter_wyvern_winters_curse") then return false end
+	if NPC.HasModifier(myHero, "modifier_bane_fiends_grip") then return false end
+	if NPC.HasModifier(myHero, "modifier_bane_nightmare") then return false end
+	if NPC.HasModifier(myHero, "modifier_faceless_void_chronosphere_freeze") then return false end
+	if NPC.HasModifier(myHero, "modifier_enigma_black_hole_pull") then return false end
+	if NPC.HasModifier(myHero, "modifier_magnataur_reverse_polarity") then return false end
+	if NPC.HasModifier(myHero, "modifier_pudge_dismember") then return false end
+	if NPC.HasModifier(myHero, "modifier_shadow_shaman_shackles") then return false end
+	if NPC.HasModifier(myHero, "modifier_techies_stasis_trap_stunned") then return false end
+	if NPC.HasModifier(myHero, "modifier_storm_spirit_electric_vortex_pull") then return false end
+	if NPC.HasModifier(myHero, "modifier_tidehunter_ravage") then return false end
+	if NPC.HasModifier(myHero, "modifier_windrunner_shackle_shot") then return false end
+	if NPC.HasModifier(myHero, "modifier_item_nullifier_mute") then return false end
+
+	return true	
+end
+
+function Shadow.IsInAbilityPhase(myHero)
+
+	if not myHero then return false end
+
+	local myAbilities = {}
+
+	for i= 0, 10 do
+		local ability = NPC.GetAbilityByIndex(myHero, i)
+		if ability and Entity.IsAbility(ability) and Ability.GetLevel(ability) > 0 then
+			table.insert(myAbilities, ability)
+		end
+	end
+
+	if #myAbilities < 1 then return false end
+
+	for _, v in ipairs(myAbilities) do
+		if v then
+			if Ability.IsInAbilityPhase(v) then
+				return true
+			end
+		end
+	end
+
+	return false
+end
+
+function Shadow.Debugger(time, npc, ability, order)
+
+	if not Menu.IsEnabled(Shadow.optionEnable) then return end
+	Log.Write(tostring(time) .. " " .. tostring(NPC.GetUnitName(npc)) .. " " .. tostring(ability) .. " " .. tostring(order))
+end
+
+function Shadow.GenericMainAttack(myHero, attackType, target, position)
 	
+	if not myHero then return end
+	if not target and not position then return end
+
+	if Shadow.isHeroChannelling(myHero) == true then return end
+	if Shadow.heroCanCastItems(myHero) == false then return end
+	if Shadow.IsInAbilityPhase(myHero) == true then return end
+
+	if Menu.IsEnabled(Shadow.optionEnable) then
+		if target ~= nil then
+			Shadow.GenericAttackIssuer(attackType, target, position, myHero)
+		end
+	else
+		Shadow.GenericAttackIssuer(attackType, target, position, myHero)
+	end
+end
+
+function Shadow.GenericAttackIssuer(attackType, target, position, npc)
+
+	if not npc then return end
+	if not target and not position then return end
+	if os.clock() - Shadow.lastAttackTime2 < 0.5 then return end
+
+	if attackType == "Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET" then
+		if target ~= nil then
+			if target ~= Shadow.LastTarget then
+				Player.PrepareUnitOrders(Players.GetLocal(), Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET, target, Vector(0, 0, 0), ability, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, npc)
+				Shadow.LastTarget = target
+				Shadow.Debugger(GameRules.GetGameTime(), npc, "attack", "DOTA_UNIT_ORDER_ATTACK_TARGET")
+			end
+		end
+	end
+
+	if attackType == "Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_MOVE" then
+		if position ~= nil then
+			if not NPC.IsAttacking(npc) or not NPC.IsRunning(npc) then
+				if position:__tostring() ~= Shadow.LastTarget then
+					Player.PrepareUnitOrders(Players.GetLocal(), Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_MOVE, target, position, ability, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, npc)
+					Shadow.LastTarget = position:__tostring()
+					Shadow.Debugger(GameRules.GetGameTime(), npc, "attackMove", "DOTA_UNIT_ORDER_ATTACK_MOVE")
+				end
+			end
+		end
+	end
+
+	if attackType == "Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION" then
+		if position ~= nil then
+			if position:__tostring() ~= Shadow.LastTarget then
+				Player.PrepareUnitOrders(Players.GetLocal(), Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, target, position, ability, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, npc)
+				Shadow.LastTarget = position:__tostring()
+				Shadow.Debugger(GameRules.GetGameTime(), npc, "move", "DOTA_UNIT_ORDER_MOVE_TO_POSITION")
+			end
+		end
+	end
+
+	if target ~= nil then
+		if target == Shadow.LastTarget then
+			if not NPC.IsAttacking(npc) then
+				Shadow.LastTarget = nil
+				Shadow.lastAttackTime2 = os.clock()
+				return
+			end
+		end
+	end
+
+	if position ~= nil then
+		if position:__tostring() == Shadow.LastTarget then
+			if not NPC.IsRunning(npc) then
+				Shadow.LastTarget = nil
+				Shadow.lastAttackTime2 = os.clock()
+				return
+			end
+		end
+	end
+end
+
 return Shadow
