918ca0af6fca98521b87118ea357c7cdffb934da
PuckItYOLO <35457186+PuckItYOLO@users.noreply.github.com>
2018-05-25 15:08:15 -0700

    More items and ranges, added Auto Self-Chakra, etc

--- Keeper.lua
+++ Keeper.lua
@@ -1,7 +1,21 @@
 local Keeper = {}
 
-Keeper.optionKey = Menu.AddKeyOption({"Hero Specific","Keeper"},"Combo Key",Enum.ButtonCode.KEY_D)
-Keeper.optionEnable = Menu.AddOption({"Hero Specific","Keeper"},"Enabled","Enable Or Disable Keeper Combo Script")
+Keeper.optionEnable = Menu.AddOption({"Hero Specific","Keeper"},"1. Enabled","Enable Or Disable Keeper Combo Script")
+Keeper.ToggleKey = Menu.AddKeyOption({"Hero Specific", "Keeper"}, "2. Auto Mode Toggle Key", Enum.ButtonCode.KEY_S)
+Keeper.optionEnableSelfChakra = Menu.AddOption({"Hero Specific", "Keeper"}, "3. Enable Self-Chakra on %", "Enable or Disable")
+Keeper.ManaRestorePoint = Menu.AddOption({"Hero Specific", "Keeper", "3.1 Set Threshold %"}, "Threshold My Mana", "Set threshold for KoTL Auto-Mana Restore", 5, 90, 5)
+Keeper.optionKey = Menu.AddKeyOption({"Hero Specific","Keeper"},"4. Combo Key",Enum.ButtonCode.KEY_D)
+Keeper.FontLarge = Renderer.LoadFont("Calibri", 24, Enum.FontWeight.EXTRABOLD)
+--Skills Toggle Menu--
+Keeper.optionEnableLeak = Menu.AddOption({"Hero Specific","Keeper","5. Skills"},"1. Use Mana Leak","Enable Or Disable")
+--Items Toggle Menu--
+Keeper.optionEnableAtos = Menu.AddOption({ "Hero Specific","Keeper","6. Items"},"1. Use Atos","Enable Or Disable")
+Keeper.optionEnableDagon = Menu.AddOption({ "Hero Specific","Keeper","6. Items"},"2. Use Dagon","Enable Or Disable")
+Keeper.optionEnableDiffusal = Menu.AddOption({ "Hero Specific","Keeper","6. Items"},"3. Use Diffusal","Enable Or Disable")
+Keeper.optionEnableForce = Menu.AddOption({ "Hero Specific","Keeper","6. Items"},"4. Use Force Staff","Enable Or Disable")
+Keeper.optionEnableNullifier = Menu.AddOption({ "Hero Specific","Keeper","6. Items"},"5. Use Nullifier","Enable Or Disable")
+
+local AutoMode = false
 
 -- global Variables
 Keeper.lastAttackTime = 0
@@ -165,10 +179,30 @@ function Keeper.GenericAttackIssuer(attackType, target, position, npc)
 	end
 end
 
+function Keeper.OnDraw()
+    local myHero = Heroes.GetLocal()
+    if not myHero or NPC.GetUnitName(myHero) ~= "npc_dota_hero_keeper_of_the_light" then return end
+    if not AutoMode then return end
+
+    --Displays when Auto Mode is toggled On--
+    local pos = Entity.GetAbsOrigin(myHero)
+    local x, y, visible = Renderer.WorldToScreen(pos)
+    Renderer.SetDrawColor(0, 255, 0, 255)
+    Renderer.DrawTextCentered(Keeper.FontLarge, x, y, "Auto-Chakra", 1)
+end
+
 function Keeper.OnUpdate()
     if not Menu.IsEnabled(Keeper.optionEnable) then return true end
 	if Menu.IsKeyDown(Keeper.optionKey)then
     Keeper.Combo()
+	end
+	
+	if Menu.IsKeyDownOnce(Keeper.ToggleKey) then
+    	AutoMode = not AutoMode
+	end
+
+	if Menu.IsEnabled(Keeper.optionEnableSelfChakra) and AutoMode then
+    	Keeper.SelfChakra()
 	end
 	
 	if not Engine.IsInGame() then
@@ -191,41 +225,111 @@ if not Menu.IsKeyDown(Keeper.optionKey) then return end
     local Leak = NPC.GetAbility(myHero, "keeper_of_the_light_mana_leak")
     
     --Item Calls--
-    local Lens = NPC.GetItem(myHero, "item_aether_lens", true)
+    local Atos = NPC.GetItem(myHero, "item_rod_of_atos", true)
+    local Diffusal = NPC.GetItem(myHero, "item_diffusal_blade", true)
     local ForceStaff  = NPC.GetItem(myHero, "item_force_staff", true)
+    local Lens = NPC.GetItem(myHero, "item_aether_lens", true)
+    local Nullifier = NPC.GetItem(myHero, "item_nullifier", true)
+    
+    local Dagon = NPC.GetItem(myHero, "item_dagon", true)
+	if not Dagon then
+		for i = 2, 5 do
+			Dagon = NPC.GetItem(myHero, "item_dagon_" .. i, true)
+			if Dagon then break end
+		end
+	end
     
     --Ability Ranges--
     local LeakRange = Ability.GetCastRange(Leak)
     
     --Item Ranges--
+    local AtosRange = 1150
+    local DagonRange = Ability.GetCastRange(Dagon)
+    local DiffusalRange = 600
   	local ForceRange = 750
+  	local NullifierRange = 600
   	
   	--Talent Tree Bonus Range-- 	
   	local TalentBonusRange = NPC.GetAbility(myHero, "special_bonus_cast_range_350")
   	
   	if Lens then
-    		LeakRange = LeakRange + 250
-    		ForceRange = ForceRange + 250
+  		AtosRange = AtosRange + 250
+  		DagonRange = DagonRange + 250
+  		DiffusalRange = DiffusalRange + 250
+  		ForceRange = ForceRange + 250
+    	LeakRange = LeakRange + 250
+    	NullifierRange = NullifierRange + 250
     end
 	
 	if TalentBonusRange and Ability.GetLevel(TalentBonusRange) > 0 then
-			LeakRange = LeakRange + 350
-    		ForceRange = ForceRange + 350
-  	end		
-	
-	if Menu.IsEnabled(Keeper.optionEnable) then
+		AtosRange = AtosRange + 350
+		DagonRange = DagonRange + 350
+		DiffusalRange = DiffusalRange + 350
+		ForceRange = ForceRange + 350
+		LeakRange = LeakRange + 350
+		NullifierRange = NullifierRange + 350	
+  	end
+	  
+	if not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_ROOTED) and Menu.IsEnabled(Keeper.optionEnableAtos) and not Entity.IsDormant(enemy) then
+		if Atos and Ability.IsCastable(Atos, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemy), AtosRange) then
+			Ability.CastTarget(Atos, enemy) return
+		end
+	end		
 		     
-	if not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE)
-	and Leak and Ability.IsReady(Leak) and Ability.IsCastable(Leak, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemy), LeakRange) then Ability.CastTarget(Leak, enemy) Keeper.GenericMainAttack(myHero, "Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET", enemy, nil) return 
+	if not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and Menu.IsEnabled(Keeper.optionEnableLeak) and not Entity.IsDormant(enemy) then
+		if Leak and Ability.IsReady(Leak) and Ability.IsCastable(Leak, mana) then
+			if NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemy), LeakRange) then
+				Ability.CastTarget(Leak, enemy) return 
+			end
+		end
 	end
 			     
-	if not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and
-	ForceStaff and Ability.IsCastable(ForceStaff, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemy), ForceRange) and NPC.HasModifier(enemy, "modifier_keeper_of_the_light_mana_leak") then Ability.CastTarget(ForceStaff, enemy) Keeper.GenericMainAttack(myHero, "Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET", enemy, nil) return
+	if not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and NPC.HasModifier(enemy, "modifier_keeper_of_the_light_mana_leak") and Menu.IsEnabled(Keeper.optionEnableForce) and not Entity.IsDormant(enemy) then
+		if ForceStaff and Ability.IsCastable(ForceStaff, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemy), ForceRange) then
+			Ability.CastTarget(ForceStaff, enemy) return
+		end
 	end
 	
-	Keeper.GenericMainAttack(myHero, "Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET", enemy, nil)
-		return
-	end 
+	if not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and Menu.IsEnabled(Keeper.optionEnableDagon) and not NPC.HasModifier(enemy, "modifier_item_blade_mail_reflect") and not NPC.HasModifier(enemy, "modifier_sheepstick_debuff") and not Entity.IsDormant(enemy) then
+		if Dagon and Ability.IsCastable(Dagon, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemy), DagonRange) then
+			Ability.CastTarget(Dagon, enemy) return
+		end
+	end
+	
+	if not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and Menu.IsEnabled(Keeper.optionEnableDiffusal) and not NPC.HasModifier(enemy, "modifier_item_diffusal_blade_slow") and not Entity.IsDormant(enemy) then
+		if Diffusal and Ability.IsCastable(Diffusal, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemy), DiffusalRange) then
+			Ability.CastTarget(Diffusal, enemy) return
+		end
+	end
+	
+	if not NPC.HasState(enemy, Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and Menu.IsEnabled(Keeper.optionEnableNullifier) and not NPC.HasModifier(enemy, "modifier_item_nullifier_mute") and not Entity.IsDormant(enemy) then
+		if Nullifier and Ability.IsCastable(Nullifier, mana) and NPC.IsPositionInRange(myHero, Entity.GetAbsOrigin(enemy), NullifierRange) then
+			Ability.CastTarget(Nullifier, enemy) return
+		end
+	end
+	
+	Keeper.GenericMainAttack(myHero, "Enum.UnitOrder.DOTA_UNIT_ORDER_ATTACK_TARGET", enemy, nil) return
+end
+
+function Keeper.SelfChakra()
+	if not Menu.IsEnabled(Keeper.optionEnable) then return end
+	local myHero = Heroes.GetLocal()
+	if NPC.GetUnitName(myHero) ~= "npc_dota_hero_keeper_of_the_light" then return end
+	local mana = NPC.GetMana(myHero)
+	
+	if not myHero then return end  
+	
+	--Ability Call--
+	local Chakra = NPC.GetAbility(myHero, "keeper_of_the_light_chakra_magic")
+	
+	if Menu.IsEnabled(Keeper.optionEnableSelfChakra) and not NPC.IsIllusion(myHero) and Keeper.heroCanCastItems(myHero) then
+		if Chakra and Ability.IsReady(Chakra) and Ability.IsCastable(Chakra, mana) then
+			local MyManaPercentage = (NPC.GetMana(myHero) / NPC.GetMaxMana(myHero)) * 100
+			if MyManaPercentage <= Menu.GetValue(Keeper.ManaRestorePoint) then
+		        Ability.CastTarget(Chakra, myHero) return
+		    end
+		end
+	end
 end
 	
 return Keeper
